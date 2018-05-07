d1b602dfc21fe1fe389e1122d403da3e15a01fa4
Cidmar da Silva Ribeiro <cidmardsr@gmail.com>
2018-05-07 11:26:58 -0300

    exercicios da lista for

---
+++ Exercicio01.java
@@ -0,0 +1,14 @@
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 11; i++){
+			
+			System.out.println(i + "X78 " + "= " +(i * 78));
+
+	}
+		
+	}
+	
+}
+	
---
+++ Exercicio02.java
@@ -0,0 +1,15 @@
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i < 11; i++){
+			for(int a = 0; a < 11; a++){
+				System.out.println(i + "X" + a + " = " + (i * a));
+			}
+			
+		}
+		
+	
+	}
+
+}
